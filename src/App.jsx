b0507674a337f73811af2e2f374845668a253b42
Abir394 <mohammadabir0172@gmail.com>
2025-02-13 19:28:04 +0600

    Error problem

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import Login from './components/Auth/Login'
-/*import EmployeDashBoard from './components/Dashboard/EmployeDashBoard'*/
+/*import Login from './components/Auth/Login'*/
+import EmployeDashBoard from './components/Dashboard/EmployeDashBoard'
 /*import Admindashboard from './components/Dashboard/Admindashboard'*/
 export const App = () => {
   return (
    <>
-   <Login/>
-   {/*<EmployeDashBoard />*/}
+   {/*<Login/>*/}
+   {<EmployeDashBoard />}
    {/*<Admindashboard />*/}
    </>
   )
